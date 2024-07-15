111454141834a2b0ff3126114f8f9579884f9e89
Dave J Nicol <46354316+davejnicol@users.noreply.github.com>
2024-07-14 21:30:42 -0400

    Added Theme Toggle to Style Guide

--- src/style-guide/sg-main.js
+++ src/style-guide/sg-main.js
@@ -24,3 +24,32 @@ function navScrollHighlighter() {
         }
     });
 }
+
+// Theme Toggle
+const themeToggle = document.querySelector("#sg-theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+    document.body.getAttribute("data-theme") === "light"
+    ? enableDarkTheme()
+    : enableLightTheme();
+});
+
+function enableDarkTheme() {
+    document.body.setAttribute("data-theme", "dark");
+    themeToggle.setAttribute("aria-label", "Switch to light theme");
+}
+
+function enableLightTheme() {
+    document.body.setAttribute("data-theme", "light");
+    themeToggle.setAttribute("aria-label", "Switch to dark theme");
+}
+
+function setThemePreference() {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      enableDarkTheme();
+      return;
+    }
+    enableLightTheme();
+}
+  
+document.onload = setThemePreference();
